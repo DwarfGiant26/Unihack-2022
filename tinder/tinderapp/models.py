49f1d76f800f3d4ad072d10d6fc4c71cb00fd349
DwarfGiant26 <albertusalvinjanitra@gmail.com>
2022-02-25 22:29:07 +1100

    database

--- tinder/tinderapp/models.py
+++ tinder/tinderapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from matplotlib.pyplot import connect
+from database import create_connection
+from register import register
 
+db_file = "../db.sqlite3"
 # Create your models here.
+
+conn = create_connection(db_file)
+
