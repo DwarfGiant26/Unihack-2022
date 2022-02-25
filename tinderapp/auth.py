734d675c1a6ac775b1998206c5a5bb24537e70ff
DwarfGiant26 <albertusalvinjanitra@gmail.com>
2022-02-26 10:39:58 +1100

    update

--- tinderapp/auth.py
+++ tinderapp/auth.py
@@ -7,14 +7,3 @@ def register(username, email, password):
         values('{username}','{email}','{password}');
     """
     execute(sql)
-
-    sql = """
-        SELECT 
-            name
-        FROM 
-            sqlite_schema
-        WHERE 
-            type ='table' AND 
-            name NOT LIKE 'sqlite_%';
-    """
-    print(query(sql))
--- tinderapp/views.py
+++ tinderapp/views.py
@@ -5,12 +5,18 @@ import tinderapp.models as models
 # Create your views here.
 def register(request):
     return render(request,'login&register/index.html')
+
 def signup(request):
     username = request.POST.get('username')
     email = request.POST.get('email')
     password = request.POST.get('password')
     auth.register(username,email,password)
     return render(request,'login&register/index.html')
+
+def login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
 
 def login(request):
     return render(request,'login&register/index.html')
