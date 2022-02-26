a2dd87540f5d6c19846731ade21648eea2993d7c
Justcarey13 <61899885+Justcarey13@users.noreply.github.com>
2022-02-26 16:45:24 +1100

    update on seting page return

--- tinderapp/views.py
+++ tinderapp/views.py
@@ -29,7 +29,7 @@ def submit_profile(request):
     max_age = request.POST.get('max_age')
     models.update_profile(email,birthday,postcode,travel_dist,interest,min_age,max_age)
 
-    return render(request,'login&register/index.html')
+    return render(request,'discovery/discovery.html')
 
 def login(request):
     if request.method == 'POST':
