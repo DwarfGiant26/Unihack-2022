ea95a31fa04c4ac45788070f01522718cb4f705f
Winnie76 <biyaoc@student.unimelb.edu.au>
2022-02-26 20:53:26 +1100

    update stats

--- tinderapp/templates/stats/stats.js
+++ tinderapp/templates/stats/stats.js
@@ -321,25 +321,25 @@
   
   !function() {
     var data = [
-      { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
-      { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
-      { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
-      { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
+      { eventName: 'Badminton with Tom', calendar: 'User2', color: 'orange' },
+      { eventName: 'Badminton with Tom', calendar: 'User2', color: 'orange' },
+      { eventName: 'Badminton with Tom', calendar: 'User2', color: 'orange' },
+      { eventName: 'Badminton with Tom', calendar: 'User2', color: 'orange' },
   
-      { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
-      { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
-      { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
-      { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
+      { eventName: 'Game vs Portalnd', calendar: 'User5', color: 'blue' },
+      { eventName: 'Game vs Houston', calendar: 'User5', color: 'blue' },
+      { eventName: 'Game vs Denver', calendar: 'User5', color: 'blue' },
+      { eventName: 'Badminton with Tom', calendar: 'User5', color: 'blue' },
   
-      { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
-      { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
-      { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
-      { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
+      { eventName: 'Badminton with Tom', calendar: 'User1', color: 'yellow' },
+      { eventName: 'Badminton with Tom', calendar: 'User2', color: 'orange' },
+      { eventName: 'Pick up from Soccer Practice', calendar: 'User3', color: 'yellow' },
+      { eventName: 'Badminton with Tom', calendar: 'User4', color: 'green' },
   
-      { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
-      { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
-      { eventName: 'Teach Kids to Code', calendar: 'Other', color: 'green' },
-      { eventName: 'Startup Weekend', calendar: 'Other', color: 'green' }
+      { eventName: 'Tennis Night', calendar: 'User4', color: 'green' },
+      { eventName: 'Bowling Team', calendar: 'User4', color: 'green' },
+      { eventName: 'Badminton with Tom', calendar: 'User4', color: 'green' },
+      { eventName: 'Badminton with Tom', calendar: 'User4', color: 'green' }
     ];
   
     
