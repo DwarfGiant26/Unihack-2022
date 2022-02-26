2bd28dbc75a3e29787882028b1706480668f4828
DwarfGiant26 <albertusalvinjanitra@gmail.com>
2022-02-26 17:40:37 +1100

    list_recommend_people

--- tinderapp/discovery.py
+++ tinderapp/discovery.py
@@ -5,26 +5,31 @@ from geopy import distance as dist
 def list_recommended_people(email):
     # get preference and location
     sql = f"""
-        select latitude,longitude,max_radius,interest
+        select latitude,longitude,max_radius,interest,min_age,max_age
         from Users
         where email='{email}';
     """
-    latitude,longitude,max_radius,interest = query(sql)[0]
+    latitude,longitude,max_radius,interest,min_age,max_age = query(sql)[0]
     
-    # get people within interest preference
+    # get people within interest and age preference
     sql = f"""
         select username,latitude,longitude,description,picture,max_radius,interest,birthday
         from Users
-        where interest like '%{interest}%' and email != '{email}';
+        where interest like '%{interest}%' 
+            and email != '{email}' 
+            and cast(strftime('%Y.%m%d', 'now') - strftime('%Y.%m%d', dob) as int) >= min_age
+            and cast(strftime('%Y.%m%d', 'now') - strftime('%Y.%m%d', dob) as int) <= max_age
+            and not people_who_like like '%{email}%';
     """
     people = query(sql)
 
-    recommended_people = []
-    # filter people that is within distance and age
-    for person in people:
-        username,other_latitude,other_longitude,description,picture,other_max_radius,interest = person
-        distance = dist.vincenty((latitude,longitude),(other_latitude,other_longitude)).km
-        if (max_radius == 'None' or distance < max_radius) and (other_max_radius == 'None' or distance < other_max_radius):
-            recommended_people.append(person)
+    recommended_people = people
+
+    # filter people that is within age
+    # for person in people:
+    #     username,other_latitude,other_longitude,description,picture,other_max_radius,interest = person
+    #     distance = dist.vincenty((latitude,longitude),(other_latitude,other_longitude)).km
+    #     if (max_radius == 'None' or distance < max_radius) and (other_max_radius == 'None' or distance < other_max_radius):
+    #         recommended_people.append(person)
     
     return recommended_people
